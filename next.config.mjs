68d1d7d96f158873e737e3a20d4e4584bdfb97d3
JatinKukreja18 <jatinkukreja18@yahoo.in>
2024-06-25 12:38:16 +0530

    next config change for static files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "/jk2024",
+    output: "export",
+    reactStrictMode: true,
+};
 
 export default nextConfig;
